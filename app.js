33a5536a947627acb31259c1edd89287c1f81ec6
martin <japresaperez@gmial.com>
2023-08-18 09:11:43 -0700

    added

---
+++ app.js
@@ -0,0 +1,8 @@
+let noun1 = 'person';
+let adjective = 'loving';
+let noun2 = 'human';
+let verb = 'fish';
+let noun3 = 'seafood';
+
+console.log(`The world's first ${noun1} was a very ${adjective}
+${noun2} who loved to ${verb} while eating ${noun3} for every meal.`);
